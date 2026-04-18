1281004b6480b5075ef4dacb0b77040e8ec53567
Jorge Turbica <jorge.turbica@gmail.com>
2026-04-19 00:22:19 +0200

    Add Caffeine cache

--- src/main/java/com/jorgeturbica/prices/application/service/PriceServiceImpl.java
+++ src/main/java/com/jorgeturbica/prices/application/service/PriceServiceImpl.java
@@ -6,6 +6,7 @@ import com.jorgeturbica.prices.domain.model.Price;
 import com.jorgeturbica.prices.domain.port.PriceRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
@@ -18,6 +19,7 @@ public class PriceServiceImpl implements PriceService {
     private final PriceRepository priceRepository;
 
     @Override
+    @Cacheable(value = "prices", key = "#productId + ':' + #brandId + ':' + #applicationDate")
     public Price getApplicablePrice(Long productId, Long brandId, LocalDateTime applicationDate) {
         log.debug("Querying applicable price for productId={}, brandId={}, date={}", productId, brandId, applicationDate);
 
